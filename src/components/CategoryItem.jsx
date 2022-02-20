5da2efa87198021f842403215ea724a76a33d445
jaffasunny <jaffer.sunny125@gmail.com>
2022-02-20 15:23:21 +0500

    Completed

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -7,16 +7,38 @@ const Container = styled.div`
 	height: 70vh;
 	position: relative;
 `;
+
 const Image = styled.img`
 	width: 100%;
 	height: 100%;
 	object-fit: cover;
 `;
+
 const Info = styled.div`
 	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
 `;
-const Title = styled.h1``;
-const Button = styled.button``;
+
+const Title = styled.h1`
+	color: white;
+	margin-bottom: 20px;
+`;
+
+const Button = styled.button`
+	border: none;
+	padding: 10px;
+	background-color: white;
+	color: gray;
+	cursor: pointer;
+	font-weight: 600;
+`;
 
 const CategoryItem = ({ item }) => {
 	console.log(item.img);
