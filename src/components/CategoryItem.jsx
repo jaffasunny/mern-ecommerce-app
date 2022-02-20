9fb84af2d421131becf8b67531593f07900ea255
jaffasunny <jaffer.sunny125@gmail.com>
2022-02-20 15:19:40 +0500

    Working on Categories and Category Item component

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -1,11 +1,34 @@
 import React from "react";
 import styled from "styled-components";
-import { categories } from "../data";
 
-const Container = styled.div``;
+const Container = styled.div`
+	flex: 1;
+	margin: 3px;
+	height: 70vh;
+	position: relative;
+`;
+const Image = styled.img`
+	width: 100%;
+	height: 100%;
+	object-fit: cover;
+`;
+const Info = styled.div`
+	position: absolute;
+`;
+const Title = styled.h1``;
+const Button = styled.button``;
 
-const CategoryItem = () => {
-	return <Container>CategoryItem</Container>;
+const CategoryItem = ({ item }) => {
+	console.log(item.img);
+	return (
+		<Container>
+			<Image src={item.img} />
+			<Info>
+				<Title>{item.title}</Title>
+				<Button>SHOP NOW</Button>
+			</Info>
+		</Container>
+	);
 };
 
 export default CategoryItem;
