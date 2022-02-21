b12c8ea4c5cbb22002bbaeee5912985dc86f08ee
jaffasunny <jaffer.sunny125@gmail.com>
2022-02-21 14:28:40 +0500

    Footer Component Completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,12 @@
-import { Facebook, GitHub, Instagram, Twitter } from "@mui/icons-material";
+import {
+	Facebook,
+	GitHub,
+	Instagram,
+	MailOutline,
+	Phone,
+	Room,
+	Twitter,
+} from "@mui/icons-material";
 import React from "react";
 import styled from "styled-components";
 
@@ -15,17 +23,61 @@ const Left = styled.div`
 
 const Logo = styled.h1``;
 
-const Desc = styled.p``;
+const Desc = styled.p`
+	margin: 20px 0px;
+`;
 
-const SocialContainer = styled.div``;
+const SocialContainer = styled.div`
+	display: flex;
+`;
 
-const SocialIcon = styled.div``;
+const SocialIcon = styled.div`
+	width: 40px;
+	height: 40px;
+	border-radius: 50%;
+	color: white;
+	background-color: #${(props) => props.color};
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	margin-right: 20px;
+`;
 
 const Center = styled.div`
 	flex: 1;
+	padding: 20px;
 `;
+
+const Title = styled.h3`
+	margin-bottom: 30px;
+`;
+
+const List = styled.ul`
+	margin: 0;
+	padding: 0;
+	list-style: none;
+	display: flex;
+	flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+	width: 50%;
+	margin-bottom: 10px;
+`;
+
 const Right = styled.div`
 	flex: 1;
+	padding: 20px;
+`;
+
+const ContactItem = styled.div`
+	margin-bottom: 20px;
+	display: flex;
+	align-items: center;
+`;
+
+const Payment = styled.img`
+	width: 50%;
 `;
 
 const Footer = () => {
@@ -40,22 +92,49 @@ const Footer = () => {
 					nisi.
 				</Desc>
 				<SocialContainer>
-					<SocialIcon>
+					<SocialIcon color="3B5999">
 						<Facebook />
 					</SocialIcon>
-					<SocialIcon>
+					<SocialIcon color="E4405F">
 						<Instagram />
 					</SocialIcon>
-					<SocialIcon>
+					<SocialIcon color="55ACEE">
 						<Twitter />
 					</SocialIcon>
-					<SocialIcon>
+					<SocialIcon color="E60023">
 						<GitHub />
 					</SocialIcon>
 				</SocialContainer>
 			</Left>
-			<Center></Center>
-			<Right></Right>
+			<Center>
+				<Title>Useful Links</Title>
+				<List>
+					<ListItem>Home</ListItem>
+					<ListItem>Cart</ListItem>
+					<ListItem>Man Fashion</ListItem>
+					<ListItem>Woman Fashion</ListItem>
+					<ListItem>Accessories</ListItem>
+					<ListItem>My Account</ListItem>
+					<ListItem>Order Tracking</ListItem>
+					<ListItem>Wishlist</ListItem>
+					<ListItem>Wishlist</ListItem>
+					<ListItem>Terms</ListItem>
+				</List>
+			</Center>
+			<Right>
+				<Title>Contact</Title>
+				<ContactItem>
+					<Room style={{ marginRight: "10px" }} /> 622 Dixie Path , South
+					Tobinchester 98336
+				</ContactItem>
+				<ContactItem>
+					<Phone style={{ marginRight: "10px" }} /> +1 234 56 78
+				</ContactItem>
+				<ContactItem>
+					<MailOutline style={{ marginRight: "10px" }} /> contact@lama.dev
+				</ContactItem>
+				<Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+			</Right>
 		</Container>
 	);
 };
