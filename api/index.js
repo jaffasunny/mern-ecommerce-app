0d5d8debaff471142e7c88c5b5ba23094584528f
jaffasunny <jaffer.sunny125@gmail.com>
2022-02-22 20:19:15 +0500

    Successfully connected to mongodb

--- api/index.js
+++ api/index.js
@@ -5,9 +5,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 mongoose
-	.connect(
-		`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lfts2.mongodb.net/shop?retryWrites=true&w=majority`
-	)
+	.connect(process.env.MONGO_URL)
 	.then(() => console.log("DB Connection success"))
 	.catch((err) => console.log(err));
 
