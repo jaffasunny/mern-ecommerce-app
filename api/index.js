b4f9d2108a598b7461e6ac6c82b4e120046fb45b
jaffasunny <jaffer.sunny125@gmail.com>
2022-02-22 20:15:12 +0500

    successully connected mongodb to the app

--- api/index.js
+++ api/index.js
@@ -1,1 +1,16 @@
-console.log('hello');
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+mongoose
+	.connect(
+		`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lfts2.mongodb.net/shop?retryWrites=true&w=majority`
+	)
+	.then(() => console.log("DB Connection success"))
+	.catch((err) => console.log(err));
+
+app.listen(5000, () => {
+	console.log("Backend server is running");
+});
